4d4acb44b61043afcd7578f583edcc01f2c61805
Danny <live626@gmail.com>
2022-04-27 15:03:23 -0400

    starting project

---
+++ src/main/java/com/albo/registroconsignatario/RegistroConsignatarioApplication.java
@@ -0,0 +1,13 @@
+package com.albo.registroconsignatario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroConsignatarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroConsignatarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albo/registroconsignatario/RegistroConsignatarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albo.registroconsignatario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroConsignatarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
